325e21d8092609d2bfa2d4e6ab3b2528102ee833
Hayden Gagnon <haydengagnon1@gmail.com>
2023-11-10 11:11:53 -0600

    re upload

--- Survival/HighScores.cs
+++ Survival/HighScores.cs
@@ -34,15 +34,55 @@ namespace PongGame
             gameCenter.BorderStyle = BorderStyle.None;
             this.Controls.Add(gameCenter);
 
-            Label listScores = new Label();
-            listScores.Text = Convert.ToString(topScoresQuery);
-            listScores.ForeColor = Color.White;
-            listScores.BackColor = Color.Black;
-            listScores.Font = new Font(gameCenter.Font.FontFamily, 28);
-            listScores.Size = new Size(SCREEN_WIDTH / 2, 100);
-            listScores.Location = new Point(250, 200);
-            listScores.BorderStyle = BorderStyle.None;
-            this.Controls.Add(listScores);
+            Label listScores1 = new Label();
+            listScores1.Text = Convert.ToString("Score:");
+            listScores1.ForeColor = Color.Yellow;
+            listScores1.BackColor = Color.Black;
+            listScores1.Font = new Font(gameCenter.Font.FontFamily, 28);
+            listScores1.Size = new Size(SCREEN_WIDTH / 2, 100);
+            listScores1.Location = new Point(250, 100);
+            listScores1.BorderStyle = BorderStyle.None;
+            this.Controls.Add(listScores1);
+
+            Label listScores2 = new Label();
+            listScores2.Text = Convert.ToString("Score:");
+            listScores2.ForeColor = Color.Yellow;
+            listScores2.BackColor = Color.Black;
+            listScores2.Font = new Font(gameCenter.Font.FontFamily, 28);
+            listScores2.Size = new Size(SCREEN_WIDTH / 2, 100);
+            listScores2.Location = new Point(250, 200);
+            listScores2.BorderStyle = BorderStyle.None;
+            this.Controls.Add(listScores2);
+
+            Label listScores3 = new Label();
+            listScores3.Text = Convert.ToString("Score:");
+            listScores3.ForeColor = Color.Yellow;
+            listScores3.BackColor = Color.Black;
+            listScores3.Font = new Font(gameCenter.Font.FontFamily, 28);
+            listScores3.Size = new Size(SCREEN_WIDTH / 2, 100);
+            listScores3.Location = new Point(250, 300);
+            listScores3.BorderStyle = BorderStyle.None;
+            this.Controls.Add(listScores3);
+
+            Label listScores4 = new Label();
+            listScores4.Text = Convert.ToString("Score:");
+            listScores4.ForeColor = Color.Yellow;
+            listScores4.BackColor = Color.Black;
+            listScores4.Font = new Font(gameCenter.Font.FontFamily, 28);
+            listScores4.Size = new Size(SCREEN_WIDTH / 2, 100);
+            listScores4.Location = new Point(250, 400);
+            listScores4.BorderStyle = BorderStyle.None;
+            this.Controls.Add(listScores4);
+
+            Label listScores5 = new Label();
+            listScores5.Text = Convert.ToString("Score:");
+            listScores5.ForeColor = Color.Yellow;
+            listScores5.BackColor = Color.Black;
+            listScores5.Font = new Font(gameCenter.Font.FontFamily, 28);
+            listScores5.Size = new Size(SCREEN_WIDTH / 2, 100);
+            listScores5.Location = new Point(250, 500);
+            listScores5.BorderStyle = BorderStyle.None;
+            this.Controls.Add(listScores5);
         }
 
         void OrderData(SQLiteConnection conn)
@@ -63,45 +103,6 @@ namespace PongGame
             sqlite_cmd.CommandText = txtQuery;
             sqlite_cmd.ExecuteNonQuery();
             sqlite_conn.Close();
-        }
-
-        DataTable ExecuteReadQuery(string query)
-        {
-            DataTable entries = new DataTable();
-
-            using (SQLiteConnection db = new SQLiteConnection("Data Source=Survival/ScoreList.db"))
-            {
-                SQLiteCommand selectCommand = new SQLiteCommand(query, db);
-                try
-                {
-                    db.Open();
-                    SQLiteDataReader reader = selectCommand.ExecuteReader();
-
-                    if (reader.HasRows)
-                        for (int i = 0; i < reader.FieldCount; i++)
-                            entries.Columns.Add(new DataColumn(reader.GetName(i)));
-
-                    int j = 0;
-                    while (reader.Read())
-                    {
-                        DataRow row = entries.NewRow();
-                        entries.Rows.Add(row);
-
-                        for (int i = 0; i < reader.FieldCount; i++)
-                            entries.Rows[j][i] = (reader.GetValue(i));
-
-                        j++;
-                    }
-
-                    db.Close();
-                }
-                catch (SQLiteException e)
-                {
-                    //OnSQLiteError(new SQLiteErrorEventArgs(e));
-                    db.Close();
-                }
-                return entries;
-            }
         }
 
         private void GoHome_KeyDown(object sender, KeyEventArgs e)
